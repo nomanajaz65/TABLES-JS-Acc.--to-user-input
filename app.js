fb77df6bf4a3abbd25d0f98137809f8b67b7eee8
nomanajaz65 <nomanajaz65@gmail.com>
2023-03-09 02:28:07 -0800

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+                 //  table of 2 
+
+// for ( var i = 0; i <= 10 ; i++){
+//     document.write(2 + " x" +" "+ i+ ' = ' + 2*i + '<br>')
+// }
+
+        //   tables user input 
+     
+// var userInp = +prompt('enter your table')
+
+// for ( var i= 0; i<11; i++){
+//     document.write(userInp + "  x" +" "+ i + " ="+ userInp*i +" " + " <br>")
+// }
